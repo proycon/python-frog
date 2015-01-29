fa2c5541c5bf6592a89d153da5d9fb08ba9b46e6
Maarten van Gompel <proycon@anaproy.nl>
2015-01-29 14:56:58 +0100

    (0.2.1) setup update for pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
 import glob
@@ -19,12 +19,21 @@ extensions = [ Extension("frog",
 
 setup(
     name = 'python-frog',
-    version = '0.1',
+    version = '0.2.1',
+    author_email = "proycon@anaproy.nl",
+    description = ("Python binding to FROG, an NLP suite for Dutch doing part-of-speech tagging, lemmatisation, morphological analysis, named-entity recognition, shallow parsing, and dependency parsing."),
+    requires = ['frog (>=0.12.20)'],
+    license = "GPL",
+    keywords = "nlp computational_linguistics dutch pos lemmatizer",
+    url = "http://proycon.github.com/clam",
     ext_modules = extensions,
     cmdclass = {'build_ext': build_ext},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Text Processing :: Linguistic",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Operating System :: POSIX",
         "Intended Audience :: Developers",
