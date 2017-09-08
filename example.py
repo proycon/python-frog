fe774d331508e9c111de17aaa87ef4e30638c3fd
Maarten van Gompel <proycon@anaproy.nl>
2017-09-09 00:03:57 +0200

    Extended example with FoLiA output

--- example.py
+++ example.py
@@ -1,9 +1,20 @@
 from __future__ import print_function, unicode_literals
 
-import frog
+from frog import Frog, FrogOptions
 
-frog = frog.Frog(frog.FrogOptions(parser=True))
+frog = Frog(FrogOptions(parser=True))
 output = frog.process_raw("Dit is een test")
 print("RAW OUTPUT=",output)
+
 output = frog.process("Dit is nog een test.")
 print("PARSED OUTPUT=",output)
+
+frog = Frog(FrogOptions(parser=True,xmlout=True))
+output = frog.process("Dit is een FoLiA test.")
+#output is now no longer a string but an instance of folia.Document, provided by the FoLiA library in PyNLPl (pynlpl.formats.folia)
+print("FOLIA OUTPUT=")
+print(output.xmlstring())
+
+print("Inspecting FoLiA output (example):")
+for word in output.words():
+    print(word.text() + " " + word.pos() + " " + word.lemma())
