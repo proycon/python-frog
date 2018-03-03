f34824f5a81a0f84af4aee63ad057d43b8dc2f5b
Maarten van Gompel <proycon@anaproy.nl>
2018-03-03 13:09:05 +0100

    Extra fix for Mac OS X compilation (explicitly use libc++)

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
+import platform
 import glob
 import os
 
@@ -14,19 +15,24 @@ if 'VIRTUAL_ENV' in os.environ:
     includedirs.insert(0,os.environ['VIRTUAL_ENV'] + '/include')
     libdirs.insert(0,os.environ['VIRTUAL_ENV'] + '/lib')
 
+if platform.system() == "Darwin":
+    extra_options = ["-stdlib=libc++"]
+else:
+    extra_options = []
+
 extensions = [ Extension("frog",
                 [ "libfolia_classes.pxd", "frog_classes.pxd", "frog_wrapper.pyx"],
                 language='c++',
                 include_dirs=includedirs,
                 library_dirs=libdirs,
                 libraries=['frog','ucto','folia'],
-                extra_compile_args=['--std=c++0x'],
+                extra_compile_args=['--std=c++0x'] + extra_options,
                 pyrex_gdb=True
                 ) ]
 
 setup(
     name = 'python-frog',
-    version = '0.3.4',
+    version = '0.3.5',
     author_email = "proycon@anaproy.nl",
     description = ("Python binding to FROG, an NLP suite for Dutch doing part-of-speech tagging, lemmatisation, morphological analysis, named-entity recognition, shallow parsing, and dependency parsing."),
     requires = ['frog (>=0.13.6)'],
