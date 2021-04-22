b4f228d9d30740f20fa87cbc82cc017036b95bad
Maarten van Gompel <proycon@anaproy.nl>
2021-04-22 14:06:27 +0200

    fixing some tests #20

--- test.py
+++ test.py
@@ -13,8 +13,8 @@ print("PARSED OUTPUT=",output)
 frog = Frog(FrogOptions(parser=True,xmlout=True))
 output = frog.process("Dit is een FoLiA test.")
 assert isinstance(output, folia.Document)
-assert isinstance(len(output.data), 1)
-assert isinstance(next(output.data.select(folia.Sentence)).text(), "Dit is een FoLiA test.")
+assert len(output.data) == 1
+assert next(output.select(folia.Sentence)).text() == "Dit is een FoLiA test."
 #output is now no longer a string but an instance of folia.Document, provided by the FoLiA library in PyNLPl (pynlpl.formats.folia)
 print("FOLIA OUTPUT=")
 print(output.xmlstring())
@@ -22,4 +22,4 @@ print(output.xmlstring())
 print("Inspecting FoLiA output (example):")
 for word in output.words():
     print(word.text() + " " + word.pos() + " " + word.lemma())
-assert len(output.words()) == 5
+assert len(list(output.words())) == 6
